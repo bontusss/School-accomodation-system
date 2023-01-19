3f998d56c6ddd6df672c89888723371afb0993e6
bontusss <ikwecheghu@gmail.com>
2023-01-20 00:16:15 +0100

    added landlord

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from app.models import Student, Course, Room, Hostel
+from app.models import Student, Course, Room, Hostel, Landlord
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(Room)
 admin.site.register(Hostel)
+admin.site.register(Landlord)
